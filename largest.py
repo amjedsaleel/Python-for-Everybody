b215472e70ababf0ff3c26bf3abf0708dee36667
Amjed Saleel K <amjedsaleel@gmail.com>
2020-09-06 21:18:23 +0530

    find largest number an list

---
+++ largest.py
@@ -0,0 +1,8 @@
+lagest_so_far = -1
+# print('Befor ', lagest_so_far)
+for the_num in [49, 412, 121, 22, 545, 30]:
+    if the_num > lagest_so_far:
+        lagest_so_far = the_num
+    # print(lagest_so_far, the_num)
+    
+print('Largest number: ', lagest_so_far)
