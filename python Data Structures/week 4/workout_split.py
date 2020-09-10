609074ef065d5b32c3424be99a70064a15bca745
Amjed Saleel K <amjedsaleel@gmail.com>
2020-09-10 11:27:26 +0530

    modified

--- python Data Structures/week 4/workout_split.py
+++ python Data Structures/week 4/workout_split.py
@@ -1,7 +1,23 @@
 mbox = open('mbox-short.txt')
+# for line in mbox:
+#     line = line.rstrip()
+#     if not line.startswith('From '):
+#         continue
+#     words = line.split()
+#     print(words[2])
+
+
+# double split
+print('')
 for line in mbox:
     line = line.rstrip()
     if not line.startswith('From '):
         continue
     words = line.split()
-    print(words[2])
+    print('Words:', words)
+    email = words[1]
+    pieces = email.split('@')
+
+    print('Email:', email)
+    print('Pieces:', pieces)
+    print('')
