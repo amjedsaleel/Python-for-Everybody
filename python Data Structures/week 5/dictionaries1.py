ba323e9ae7827e01d2df796fe4f87b5cd9d4b8d8
Amjed Saleel K <amjedsaleel@gmail.com>
2020-09-10 17:27:48 +0530

    Week 5

---
+++ python Data Structures/week 5/dictionaries1.py
@@ -0,0 +1,17 @@
+details = dict()
+
+details['Name'] = 'Amjed'
+details['Age'] = 22
+details['Place'] = 'Calicut'
+
+details['Name'] = details['Name'] + ' Saleel'
+
+print(details)
+print(details['Name'])
+
+
+# dictionaries constants
+a = {'Name': 'Danwand', 'Age': 20}
+a['Place'] = 'Calicut'
+print(a)
+
